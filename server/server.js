6390f1248d84b6bdbd9c697498b7977580b8af4f
Ravina-Bisht <ravinabisht04@gmail.com>
2026-06-10 17:31:17 +0530

    Fix CORS for Vercel

--- server/server.js
+++ server/server.js
@@ -51,8 +51,11 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+       "http://localhost:5173",
+      "https://dmio-web.vercel.app",
     ],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
